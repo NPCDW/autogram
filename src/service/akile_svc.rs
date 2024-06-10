1aa2300f0c094bb5723e3f3613adda5e9292273d
NPCDW <32638459+NPCDW@users.noreply.github.com>
2024-06-10 10:20:40 +0000

    akile

--- src/service/akile_svc.rs
+++ src/service/akile_svc.rs
@@ -10,8 +10,12 @@ pub async fn checkin(init_data: InitData) -> anyhow::Result<()> {
         return Err(anyhow!("AKILE_CHAT_ID 环境变量配置错误，跳过 akile 签到， AKILE_CHAT_ID: {:?}", akile_chat_id));
     }
     let akile_chat_id = akile_chat_id.unwrap().parse().unwrap();
-    super::chat_svc::chat(init_data, ChatArgs {
+    super::chat_svc::chat(init_data.clone(), ChatArgs {
         chat_id: akile_chat_id,
         message: "/checkin@akilecloud_bot".to_string(),
+    }).await?;
+    super::chat_svc::chat(init_data, ChatArgs {
+        chat_id: akile_chat_id,
+        message: "煞笔6b".to_string(),
     }).await
 }
