4fef7417766b4251d8c59bf40f80a6c901c927ee
NPCDW <chenyongli0520@gmail.com>
2025-06-09 16:29:39 +0800

    .

--- src/service/signal_svc.rs
+++ src/service/signal_svc.rs
@@ -1,15 +1,18 @@
 use std::process;
 use tokio::signal::ctrl_c;
-use tokio::signal::unix::{signal, SignalKind};
 
 pub fn handle() {
-    tokio::spawn(async {
-        let mut sig = signal(SignalKind::terminate()).expect("failed to listen for SIGTERM signal");
-        sig.recv().await;
-        tracing::info!("got SIGTERM signal. autogram is exiting");
-        process::exit(0);
-    });
-    
+    #[cfg(not(windows))]
+    {
+        use tokio::signal::unix::{signal, SignalKind};
+        tokio::spawn(async {
+            let mut sig = signal(SignalKind::terminate()).expect("failed to listen for SIGTERM signal");
+            sig.recv().await;
+            tracing::info!("got SIGTERM signal. autogram is exiting");
+            process::exit(0);
+        });
+    }
+
     tokio::spawn(async {
         ctrl_c().await.expect("failed to listen for ctrl_c event");
         tracing::info!("got CTRL-C. autogram is exiting");
